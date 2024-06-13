5d31b8384f53f61da51272d84db41e461c63f87a
Simon-jmk <151679315+Simon-jmk@users.noreply.github.com>
2024-06-13 11:44:49 +0200

    removed jwt token expire

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -17,9 +17,7 @@ export default async (req, res) => {
     return res.status(401).json({ error: "Invalid email or password" });
   }
 
-  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
 
   res.status(200).json({ token });
 };
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -25,9 +25,7 @@ export default async (req, res) => {
       },
     });
 
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
 
     res.status(201).json({ token });
   } catch (error) {
